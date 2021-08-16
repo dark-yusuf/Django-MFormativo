f8196ef3a827e102ef98938c29cfb3762b44fb9d
Yusuf <dark_yusuf@outlook.com>
2021-08-15 21:58:43 -0700

    Material unidad II

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,5 +3,7 @@ from django.urls.resolvers import URLPattern
 from . import views
 urlpatterns=[
     path('', views.index),
-    path('contacto/' , views.contacto)
+    path('contacto/' , views.contacto),
+    path('recibir/', views.recibir),
+    path('sumar/<int:numero1>/<int:numero2>/', views.sumar)
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -5,5 +5,16 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'catalogo/index.html')
 
+
+def recibir(request):
+    print(request.GET["data"])
+    numero=request.GET["data"]
+    return render(request, 'catalogo/index.html',{"numero":numero})
+
+def sumar(request, numero1, numero2):
+    print(numero1, numero2)
+    resultado= numero1+numero2
+    return render(request, 'catalogo/index.html', {"numero": resultado})
+
 def contacto(request):
     return render(request, 'catalogo/contacto.html')
